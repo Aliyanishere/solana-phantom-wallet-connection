12fcbb602d84f4f875a08cb93049df834e307425
Aliyanatesspfa <aliyan@esspfa.com>
2023-05-22 12:22:23 +0500

    Transforming Tokens

--- src/components/account-info/index.jsx
+++ src/components/account-info/index.jsx
@@ -2,11 +2,55 @@
 import { useEffect, useState } from "react";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { Connection, PublicKey } from "@solana/web3.js";
+import {
+  createMint,
+  getAccount,
+  getMint,
+  getOrCreateAssociatedTokenAccount,
+} from "@solana/spl-token";
+import { clusterApiUrl, Keypair } from "@solana/web3.js";
+import { Buffer } from "buffer";
+global.Buffer = Buffer;
 
 const AccountInfo = () => {
   const [balance, setBalance] = useState(null);
   const [tokenAccounts, setTokenAccounts] = useState([]);
   const { publicKey } = useWallet();
+  const payer = Keypair.generate();
+  const mintAuthority = Keypair.generate();
+  const freezeAuthority = Keypair.generate();
+
+  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+  const mint = createMint(
+    connection,
+    payer,
+    mintAuthority.publicKey,
+    freezeAuthority.publicKey,
+    9 // We are using 9 to match the CLI decimal default exactly
+  );
+
+  mint.then((res) => console.log(res)).catch((err) => console.log(err));
+
+  console.log("mint", mint);
+
+  const mintInfo = getMint(connection, mint);
+
+  console.log("mintInfo", mintInfo.supply);
+
+  const tokenAccount = getOrCreateAssociatedTokenAccount(
+    connection,
+    payer,
+    mint,
+    payer.publicKey
+  );
+
+  console.log("tokenAccount.address", tokenAccount.address);
+
+  const tokenAccountInfo = getAccount(connection, tokenAccount.address);
+
+  console.log("amount", tokenAccountInfo.amount);
+  // 0
 
   useEffect(() => {
     if (!publicKey) return;
@@ -44,8 +88,8 @@ const AccountInfo = () => {
             <ul>
               {tokenAccounts.map((account) => (
                 <li key={account.pubkey.toBase58()}>
-                  {account.account.tokenAmount.amount}{" "}
-                  {account.account.tokenAmount.uiAmountString}{" "}
+                  {account.account.tokenAmount.amount}
+                  {account.account.tokenAmount.uiAmountString}
                   {account.account.mint.symbol}
                 </li>
               ))}
